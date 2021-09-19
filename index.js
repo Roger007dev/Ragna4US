e966ba90facde46de60dbe2335b34c2b9b802a60
Roger007dev <88948885+Roger007dev@users.noreply.github.com>
2021-09-19 03:56:55 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-/*const express = require('express')
+const express = require('express')
 const path = require('path')
 const PORT = process.env.PORT || 5000
 
@@ -7,19 +7,4 @@ express()
   .set('views', path.join(__dirname, 'views'))
   .set('view engine', 'ejs')
   .get('/', (req, res) => res.render('pages/index'))
-  .listen(PORT, () => console.log(`Listening on ${ PORT }`))*/
-
-const http = require('http');
-const port = process.env.PORT || 3000
-const path = require('path')
-const express = require('express')
-
-const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/html');
-  res.end('<h1>Hello World</h1>');
-});
-
-server.listen(port,() => {
-  console.log(Server running at port +port);
-});
+  .listen(PORT, () => console.log(`Listening on ${ PORT }`))
